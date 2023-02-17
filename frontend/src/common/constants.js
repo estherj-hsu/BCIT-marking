f2fe688cfd411bb874a7c9f885d013dae12b5d14
Esther <estherhsu.j@gmail.com>
2023-02-17 10:24:11 -0800

    frontend[add]: common/constants

---
+++ frontend/src/common/constants.js
@@ -0,0 +1,20 @@
+export const ASSIGNMENTS = [
+  {
+    question: 'When was BCIT’s 50th-anniversary celebration?',
+    options: ['2016', '1967', '2017', '1987'],
+    type: 'radio',
+    idx: 0,
+  },
+  {
+    question: 'Which of the following services does the LTC provide? Select all that apply.',
+    options: ['Technical illustration', 'Instructional design', 'Financial advice', 'Admission and Registration', 'Audio-visual loans'],
+    type: 'checkbox',
+    idx: 1,
+  },
+  {
+    question: 'The current Prime Minister in Canada is (include the starting year for the PM) ',
+    options: [],
+    type: 'input',
+    idx: 2,
+  }
+];
